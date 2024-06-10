804e53e02a9162da8b6b6ac852d1d0f8a24a30c0
JacobDaleOlguin <JacobDaleOlguin@gmail.com>
2024-06-10 13:41:11 -0400

    added portfolio finally fixed ios

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './components/Home'
 import About from './components/About'
-import Contact from './components/Contact'
 import Portfolio from './components/Portfolio'
+import Contact from './components/Contact'
+
 
 function App() {
   return (
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,3 +1,18 @@
-const Portfolio = () => 'Portfolio Page'
+import './index.scss'
+
+const Portfolio = () => {
+  return (
+    <>
+      <div className="container portfolio-page">
+        <div className="port-grid">
+          <div className="port-box">Still working on this</div>
+          <div className="port-box">Still working on this</div>
+          <div className="port-box">Still working on this</div>
+          <div className="port-box">Still working on this</div>
+        </div>
+      </div>
+    </>
+  )
+}
 
 export default Portfolio
